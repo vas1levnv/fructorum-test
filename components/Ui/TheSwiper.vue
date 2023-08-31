10efca81b59104017cf433600a0b682435a5279d
vas1levnv <vas1levnv@yandex.ru>
2023-08-31 13:36:32 +0300

    swiper

--- components/Ui/TheSwiper.vue
+++ components/Ui/TheSwiper.vue
@@ -1,12 +1,19 @@
 <script setup>
 import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Pagination, Navigation } from 'swiper/modules';
 import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 const props = defineProps(["slides"])
 </script>
 
 <template lang="pug">
-swiper(:space-between="50")
+swiper(
+  :space-between="50"
+  :navigation="true"
+  :pagination="{type: 'fraction'}"
+  )
   swiper-slide(v-for="el in props.slides")
     img(:src="el" alt="image")
 </template>
