777d5cdc47c0cdc678a8671d9af6ef94c8a5b7cd
vas1levnv <vas1levnv@yandex.ru>
2023-08-31 13:27:45 +0300

    swiper pug

--- components/Ui/TheSwiper.vue
+++ components/Ui/TheSwiper.vue
@@ -2,19 +2,13 @@
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import 'swiper/css';
 
-const props = defineProps({
-  slides: Array,
-})
+const props = defineProps(["slides"])
 </script>
 
-<template>
-  <swiper
-      :space-between="50"
-  >
-    <swiper-slide v-for="el in props.slides">
-      <img :src="el" alt="image">
-    </swiper-slide>
-  </swiper>
+<template lang="pug">
+swiper(:space-between="50")
+  swiper-slide(v-for="el in props.slides")
+    img(:src="el" alt="image")
 </template>
 
 <style scoped lang="scss">
